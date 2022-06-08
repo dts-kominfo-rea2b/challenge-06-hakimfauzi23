9f10abf405a7adf6c5d888adbb412056cf91c2ba
Hanif <hanif.hakim@spesolution.com>
2022-06-08 14:44:06 +0700

    Completed Challenge

--- coba.js
+++ coba.js
@@ -1,4 +1,3 @@
-// TODO: import module bila dibutuhkan di sini
 const fs = require("fs");
 
 // ! JANGAN DIMODIFIKASI
@@ -6,26 +5,7 @@ let file1 = "./data1.json";
 let file2 = "./data2.json";
 let file3 = "./data3.json";
 
-// ! JANGAN DIMODIFIKASI
-let modifyFile1 = (val) => {
-  file1 = val;
-};
-let modifyFile2 = (val) => {
-  file2 = val;
-};
-let modifyFile3 = (val) => {
-  file3 = val;
-};
-
-// TODO: Kerjakan bacaData
-// gunakan variabel file1, file2, dan file3
-
-
 const dataGenerator = (rawData) => {
-
-  // Fungsi untuk mengambil kata terakhir dari pesan yg sudah dibaca
-  // Setiap kondisi spesifik dengan setiap file yang akan dibaca
-
   if (rawData.message !== undefined) {
     let processedData = rawData.message.split(" ");
     return processedData[processedData.length - 1];
@@ -42,16 +22,13 @@ const dataGenerator = (rawData) => {
   }
 };
 
-
 async function bacaData(fnCallback) {
   let fileList = [file1, file2, file3];
   let data = [];
 
   try {
-    // Menggunakan for of, karena await tidak akan bs berjalan apabila menggunakan for each.
     for (const e of fileList) {
       const readedData = await fs.promises.readFile(e, "utf-8");
-      // menggunakan fungsi dataGenerator untuk mendapatkan kata terakhir.
       data.push(dataGenerator(JSON.parse(readedData)));
     }
     fnCallback(null, data);
@@ -60,10 +37,23 @@ async function bacaData(fnCallback) {
   }
 }
 
-// ! JANGAN DIMODIFIKASI
-module.exports = {
-  modifyFile1,
-  modifyFile2,
-  modifyFile3,
-  bacaData,
-};
+bacaData();
+
+// async function readingDirectory(directory) {
+//   const fileNames = await fs.promises.readdir(directory);
+//   for (let file of fileNames) {
+//     const absolutePath = path.join(directory, file);
+//     log(absolutePath);
+
+//     const data = await fs.promises.readFile(absolutePath);
+//     log(data);
+//   }
+// }
+
+// readingDirectory(folder)
+//   .then(() => {
+//     log("all done");
+//   })
+//   .catch((err) => {
+//     log(err);
+//   });
